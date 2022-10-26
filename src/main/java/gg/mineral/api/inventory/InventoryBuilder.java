a779f36b9ce7d10607fd19233dcad0b5fa86d0d1
Jaiden <jaibrown51@gmail.com>
2022-10-26 21:50:25 +0100

    Add interaction predicate to InventoryBuilder

--- src/main/java/gg/mineral/api/inventory/InventoryBuilder.java
+++ src/main/java/gg/mineral/api/inventory/InventoryBuilder.java
@@ -1,5 +1,9 @@
 package gg.mineral.api.inventory;
 
+import java.util.function.Predicate;
+
 public interface InventoryBuilder<T> {
   public MineralInventory build(T obj);
+
+  public MineralInventory build(T obj, Predicate<Interaction> interactionPredicate);
 }
